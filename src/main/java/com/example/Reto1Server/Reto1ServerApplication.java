f8407ab3e301df71c560126dc8985ce97b40f676
R <robson.garciaju@elorrieta-errekamari.com>
2023-10-18 15:33:18 +0200

    Server creado sin configurar

---
+++ src/main/java/com/example/Reto1Server/Reto1ServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Reto1Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto1ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto1ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Reto1Server/Reto1ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Reto1Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto1ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
